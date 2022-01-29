c083e8a1059c9895e535bea73ac97dd5b97e4a8a
bekkikhuman <bikramkhuman1@gmail.com>
2022-01-29 16:44:47 +0530

    fizz buzz

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log("hello world");
-    
+for
+
+(let i = 0; i <= 100; i++) {
+
+    if (i % 3 === 0) console.log("fizz");
+    else if (i % 5 === 0) console.log("buzz");
+    else console.log(i);
+
+
+}
