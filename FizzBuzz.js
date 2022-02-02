b4abe50e8a6b4253914bbb65b7f37868b7d5398c
bekkikhuman <bikramkhuman1@gmail.com>
2022-02-02 21:06:46 +0530

    FizzBuzz: output in a single line

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,6 +1,9 @@
+var str = "";
+
 for (let i = 1; i <= 100; i++) {
-    if (i % 5 === 0 && i % 3 === 0) console.log("FizzBuzz ");
-    else if (i % 3 === 0) console.log("Fizz ");
-    else if (i % 5 === 0) console.log("Buzz ");
-    else console.log(i);
+    if (i % 5 === 0 && i % 3 === 0) str += ("FizzBuzz ");
+    else if (i % 3 === 0) str += ("Fizz ");
+    else if (i % 5 === 0) str += ("Buzz ");
+    else str += i + " ";
 }
+console.log(str);
